c868992c6c5b6e1fe05081aa72df69d31ad267f6
jkropko <jkropko@gmail.com>
2025-12-02 15:15:28 -0500

    Added the voting scatterplot

--- app/app.py
+++ app/app.py
@@ -59,7 +59,8 @@ app.layout = html.Div(
                          select them in the dropbox box below. If you want 
                          to lookup who your representatives are, check out 
                          [this website](https://www.congress.gov/members/find-your-member).'''),
-            dcc.Dropdown(id = 'dropdown', options=dropdown_options) # Step 1: User input
+            dcc.Dropdown(id = 'dropdown', options=dropdown_options, 
+                         value='N000188') # Step 1: User input
         ],
                  style={'width':'25%', 'float':'left'}),
 
@@ -67,10 +68,13 @@ app.layout = html.Div(
             dcc.Tabs([
                 dcc.Tab(label = 'Biographical Information',
                         children = [
-                            dcc.Graph(id = 'biotable') # Step 5: place output on the dashboard
+                            html.Div([html.Img(id = 'bioimage')], style={'width':'20%', 'float':'left'}),
+                            html.Div([dcc.Graph(id = 'biotable')], style={'width':'80%', 'float':'right'})
                         ]),
                 dcc.Tab(label = 'How They Vote',
-                        children = []),
+                        children = [
+                            dcc.Graph(id='vote_scatter')
+                        ]),
                 dcc.Tab(label = 'Sponsored Bills',
                         children = []),
                 dcc.Tab(label = 'Who is Giving Them Money?',
@@ -99,6 +103,49 @@ def biotable(b): # Step 3: write a function that turns input into output
     member_info = member_info.drop(['bioguide_id', 'image', 'fec_id', 'bioname', 'icpsr'],
         axis=1)
     return [ff.create_table(member_info.T.reset_index().rename({'index':'',0:''}, axis=1))]
+
+@app.callback([Output(component_id = 'bioimage', component_property='src')], 
+              [Input(component_id = 'dropdown', component_property = 'value')])
+
+def bioimage(b):
+    
+    myquery = f'''
+        SELECT image
+        FROM members
+        WHERE bioguide_id = '{b}'
+    '''
+    image_url = pd.read_sql_query(myquery, con=engine)['image'][0]
+    return [image_url]
+
+@app.callback([Output(component_id = 'vote_scatter', component_property='figure')], 
+              [Input(component_id = 'dropdown', component_property = 'value')])
+
+def vote_scatterplot(b):
+    myquery = f'''
+        SELECT c.comparison_member,
+            c.agree,
+            m.left_right_ideology,
+            m.party
+        FROM members m
+        INNER JOIN (
+            SELECT vc.comparison_member,
+                vc.agree
+            FROM members m
+            INNER JOIN vote_compare vc
+                ON m.bioname = vc.bioname
+            WHERE m.bioguide_id = '{b}'
+        ) c
+            ON m.bioname = c.comparison_member
+
+        '''
+    vote_data = pd.read_sql_query(myquery, con=engine)
+    fig = px.scatter(vote_data,
+                 x = 'left_right_ideology',
+                 y = 'agree',
+                 hover_name = 'comparison_member',
+                 color = 'party',
+                 color_discrete_map={'Democrat': 'blue','Republican': 'red'})
+    return [fig]
 
 # Run the dashboard
 if __name__=='__main__':
